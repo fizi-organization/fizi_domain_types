b8d33868a2bde04c929c7cfd857d2c27c4c27ac9
Bruno Paixão <ppbruno.paixao@gmail.com>
2026-05-09 14:40:41 -0300

    fix: export shared contracts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,7 @@
 export * from './exercicios';
 export * from './programas';
+export * from './appointments';
+export * from './chat';
+export * from './notifications';
+export * from './physio';
+export * from './patients';
